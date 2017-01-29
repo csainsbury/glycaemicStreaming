bfdea7361dd5d2f4729681b0995ac4a22e95b860
chris sainsbury <chris@sainsbury.im>
2017-01-29 17:21:10 +0000

    .

--- _glycaemicStreaming.R
+++ _glycaemicStreaming.R
@@ -23,16 +23,17 @@ a1c_dataDT<-a1c_dataDT[order(a1c_dataDT$LinkId,a1c_dataDT$dateplustime1),]
 
 # DM subset for extracting hba1c data
 T1DM_sub <- diagnosisDatasetDT[DiabetesMellitusType_Mapped == "Type 1 Diabetes Mellitus"]
+T2DM_sub <- diagnosisDatasetDT[DiabetesMellitusType_Mapped == "Type 2 Diabetes Mellitus"]
 
-T1DM_sub_test <- data.frame(diagnosisDatasetDT$LinkId, diagnosisDatasetDT$DateOfDiagnosisDiabetes_Date)
+T1DM_sub <- T2DM_sub
+
+T1DM_sub_test <- data.frame(T1DM_sub$LinkId, T1DM_sub$DateOfDiagnosisDiabetes_Date)
 colnames(T1DM_sub_test) <- c("LinkId", "diagnosisDate")
 T1DM_sub_test$diagnosisDateUnix <- returnUnixDateTime(T1DM_sub_test$diagnosisDate)
 
 T1DM_sub_test_a1c_merge <- merge(T1DM_sub_test, a1c_dataDT, by.x="LinkId", by.y="LinkId")
 T1DM_sub_test_a1c_merge$interval_months <- (T1DM_sub_test_a1c_merge$dateplustime1 - T1DM_sub_test_a1c_merge$diagnosisDateUnix) / (60*60*24*(365.25/12))
 T1DM_sub_test_a1c_merge <- data.table(T1DM_sub_test_a1c_merge)
-
-
 
 findHbA1cValues <- function(dateplustime1, newNumeric, diagnosisDateUnix, timePoint_1, timePoint_2, window_1, window_2) {
   
@@ -69,9 +70,9 @@ findHbA1cValues <- function(dateplustime1, newNumeric, diagnosisDateUnix, timePo
   return(list(value_1, value_2))
 }
 
-timePoint_1 = 12 # months
+timePoint_1 = 24 # months
 timePoint_2 = 60 # months
-window_1 = 2     # window (half)
+window_1 = 4     # window (half)
 window_2 = 6     # window (half)
 
 T1DM_sub_test_a1c_merge[, c("a1c_point1", "a1c_point2") := findHbA1cValues(dateplustime1, newNumeric, diagnosisDateUnix, timePoint_1, timePoint_2, window_1, window_2) , by=.(LinkId)]
@@ -80,10 +81,17 @@ T1DM_sub_test_a1c_merge <- T1DM_sub_test_a1c_merge[order(T1DM_sub_test_a1c_merge
 T1DM_sub_test_a1c_merge$idDiff <- 1
 T1DM_sub_test_a1c_merge$idDiff[2:nrow(T1DM_sub_test_a1c_merge)] <- diff(T1DM_sub_test_a1c_merge$LinkId)
 
+# bring down to 1 row per ID
 T1DM_sub_test_a1c_merge_plot <- T1DM_sub_test_a1c_merge[idDiff==1]
 
+# sub with values at both time points:
+a1c_2point_plot <- T1DM_sub_test_a1c_merge_plot[a1c_point1>0 & a1c_point2>0]
 
-
+# 
+plot(a1c_2point_plot$a1c_point1, a1c_2point_plot$a1c_point2)
+  fit <- lm(a1c_2point_plot$a1c_point2 ~ a1c_2point_plot$a1c_point1)
+  abline(fit, col="red")
+boxplot(a1c_2point_plot$a1c_point2 ~ cut(a1c_2point_plot$a1c_point1, breaks=seq(0,200,5)), varwidth=T, xlab=paste("a1c at ",timePoint_1," months", sep=""), ylab=paste("a1c at ",timePoint_2," months", sep=""), main=paste("glycemic streaming. T1DM. n=",nrow(a1c_2point_plot), sep=""))
 
 
 
